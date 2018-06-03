3a1ab96b786a752042a624bf3ee9b4d36c42c13d
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-06-03 15:26:59 -0400

    Functional UI and methods

--- Virtual Pet Project/Program.cs
+++ Virtual Pet Project/Program.cs
@@ -10,7 +10,106 @@ namespace Virtual_Pet_Project
     {
         static void Main(string[] args)
         {
+            VirtualPet Pet = new VirtualPet("Waggy", 15, 12, 7);
+            int hungerLevel = 0;
+            int sleepyLevel = 0;
+            int happyLevel = 0;
+            int userSelection;
+            string userActionFeed;
+            string userActionSleep;
+            string userActionPlay;
+
+
+            Console.WriteLine("Your pet's name is {0}.", Pet.Name);
+            Console.WriteLine("Your pet's hunger level is currently at {0}. Your pet's sleepiness level is currently at {1}. Your pet's happiness level is currently at {2}.", Pet.HungerLevel, Pet.SleepyLevel, Pet.HappyLevel);
+            Console.WriteLine("Type 1 to feed pet. Type 2 to give pet a nap. Type 3 to play with pet. ");
+            userSelection = int.Parse(Console.ReadLine());
+
+
+            if (userSelection == 1)
+            {
+                Pet.Feed();
+                Console.WriteLine("Your pet's new hunger level is {0}.", Pet.HungerLevel);
+
+            }
+
+            else if (userSelection == 2)
+            {
+                Pet.Sleep();
+                Console.WriteLine("Your pet's new sleep level is {0}.", Pet.SleepyLevel);
+            }
+
+            else if (userSelection == 3)
+            {
+                Pet.Play();
+                Console.WriteLine("Your pet's new happy level is {0}.", Pet.HappyLevel);
+            }
+
+            if (hungerLevel > 10)
+            {
+                Console.WriteLine("Your pet is hungry. Would you like to feed it?");
+                userActionFeed = Console.ReadLine().ToLower();
+                if (userActionFeed == "yes")
+                {
+                    Pet.Feed();
+                    Console.WriteLine("Your pet's new hunger level is {0}.", Pet.HungerLevel);
+                }
+
+                else
+                {
+                    
+                }
+            }
+
+            if (sleepyLevel < 10)
+            {
+                Console.WriteLine("Your pet is sleepy. Would you like to give it a nap?");
+                userActionSleep = Console.ReadLine().ToLower();
+                if (userActionSleep == "yes")
+                {
+                    Pet.Sleep();
+                    Console.WriteLine("Your pet's new sleep level is {0}.", Pet.SleepyLevel);
+                }
+
+                else
+                {
+                    
+                }
+            }
+
+            if (happyLevel < 10)
+            {
+                Console.WriteLine("Your pet is sad. Would you like to play with it?");
+                userActionPlay = Console.ReadLine().ToLower();
+                if (userActionPlay == "yes")
+                {
+                    Pet.Play();
+                    Console.WriteLine("Your pet's new happy level is {0}.", Pet.HappyLevel);
+                }
+
+                else
+                {
+                    
+                }
+            }
+
+
+
+
+
+
+
+
+
 
         }
+
+
+
+
+
+
+
+
     }
 }
